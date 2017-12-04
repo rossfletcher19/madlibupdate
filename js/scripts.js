38219531fcd4f2fca0c8be57ebfb6a5de19f7d97
Kyle Tenzeldam and Ross Fletcher <ross@>
2017-12-04 09:30:05 -0800

    add looping to js format

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 $(function() {
  $("#madLib").submit(function(event) {
-   var firstName = $("input#firstName").val();
-   var lastName = $("input#lastName").val();
-   var verb = $("input#verb").val();
-   var noun = $("input#noun").val();
-   var city = $("input#city").val();
+var blanks = ["firstName", "lastName", "verb", "noun", "city"];
 
-   $(".firstName").append(firstName);
-   $(".lastName").append(lastName);
-   $(".verb").append(verb);
-   $(".noun").append(noun);
-   $(".city").append(city);
+  //  var firstName = $("input#firstName").val();
+  //  var lastName = $("input#lastName").val();
+  //  var verb = $("input#verb").val();
+  //  var noun = $("input#noun").val();
+  //  var city = $("input#city").val();
+
+blanks.forEach(function(blank) {
+  var userInput = $("input#" + blank).val();
+  $("." + blank).text(userInput);
+});
+  //  $(".firstName").append(firstName);
+  //  $(".lastName").append(lastName);
+  //  $(".verb").append(verb);
+  //  $(".noun").append(noun);
+  //  $(".city").append(city);
 
    $("#story").show();
 
