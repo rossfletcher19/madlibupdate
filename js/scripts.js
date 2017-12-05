2df0e2ea660bac06cd4f84a0c755ce423a77a99e
julia sheremet and Ross Fletcher <ross@>
2017-12-05 09:48:42 -0800

    add for method to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,17 @@
 $(function() {
  $("#madLib").submit(function(event) {
    var blanks = ["firstName", "lastName", "verb", "noun", "city"];
+   for (var index = 0; index < blanks.length; index += 1){
+     var word = blanks[index]
+     var userInput = $("input#" + word).val();
+     $("." + word).text(userInput);   //targeting class labeled "firstname"
+   }
 
-  //  var firstName = $("input#firstName").val();
-  //  var lastName = $("input#lastName").val();
-  //  var verb = $("input#verb").val();
-  //  var noun = $("input#noun").val();
-  //  var city = $("input#city").val();
+    // blanks.forEach(function(blank) {
+    //   var userInput = $("input#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
 
-    blanks.forEach(function(blank) {
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
-    });
-  //  $(".firstName").append(firstName);
-  //  $(".lastName").append(lastName);
-  //  $(".verb").append(verb);
-  //  $(".noun").append(noun);
-  //  $(".city").append(city);
 
    $("#story").show();
    event.preventDefault();
--- js/scripts1.js
+++ js/scripts1.js
@@ -1,12 +1,17 @@
 $(function() {
   $("#madlib").submit(function(event) {
-    // debugger;
-    var blanks = ["firstName", "lastName", "verb", "noun", "city"];
+    debugger;
+    var blanks = [$("#firstName").val(), $("#lastName").val(), $("#verb").val(), $("#noun").val(), $("#city").val()];
+    for (var index = 0; index < blanks.length; index += 1){
+      var word = blanks[index]
+    }
 
-    blanks.forEach(function(blank) {
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
-    });
+    // blanks.forEach(function(blank) {
+    //   var userInput = $("input#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
+
+
 
     $("#story").show();
     event.preventDefault();
